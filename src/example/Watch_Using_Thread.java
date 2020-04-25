d474e9538f6dca4e12e8e59bd0eae13d65ff4f34
naimul <naimulhaquesagar24@gmail.com>
2020-04-25 20:39:13 +0600

    code added

--- src/example/Watch_Using_Thread.java
+++ src/example/Watch_Using_Thread.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package example;
 
-/**
- *
- * @author naimu
- */
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Watch_Using_Thread {
-    
+
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            while (true) {
+                printWatch();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Watch_Using_Thread.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        t.start();
+    }
+
+    private static void printWatch() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:s a");
+        String formattedTime = LocalDateTime.now().format(formatter);
+        System.out.println(formattedTime);
+    }
 }
