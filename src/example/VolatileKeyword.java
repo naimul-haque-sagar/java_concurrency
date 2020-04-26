cfaaa6324c0ad488aeea7d991e4ec6386a2a3e25
naimul <naimulhaquesagar24@gmail.com>
2020-04-26 11:15:35 +0600

    code added

---
+++ src/example/VolatileKeyword.java
@@ -0,0 +1,27 @@
+package example;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class VolatileKeyword {
+
+    static volatile boolean flag = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            while (flag) {
+                System.out.println("Into " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(VolatileKeyword.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            System.out.println("Thread stopped");
+        }).start();
+
+        Thread.sleep(2000);
+        flag = false;
+        System.out.println("finished");
+    }
+}
